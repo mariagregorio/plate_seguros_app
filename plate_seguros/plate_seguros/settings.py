81993577a840b892d2928fdee49b84dbbbfe933c
maria gregorio <airam.greg@gmail.com>
2021-11-10 12:28:15 -0300

    cambios en media files

--- plate_seguros/plate_seguros/settings.py
+++ plate_seguros/plate_seguros/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
-MEDIA_URL = '/assets/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
